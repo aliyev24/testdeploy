6241da2bca0811a0f1ebbea5f58a9d06f1f1b3da
avaz <[200~aliyev.avaz.24@gmail.comgit config --global user.email aliyev.avaz.24@gmail.comgit config --global user.email [200~aliyev.avaz.24@gmail.com>
2026-08-20 12:03:07 +0400

    changes

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -18,9 +18,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
